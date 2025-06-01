12d878021540a2405a3940f2c9c96fd56ede1a5e
Haylin Moore <me@haylinmoore.com>
2025-06-01 11:31:15 -0700

    users(root): add infra SSH key

--- modules/shared/users.nix
+++ modules/shared/users.nix
@@ -5,6 +5,7 @@
     root = {
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHavg+rhFmR2p9wuWiO4VxKaIXpq1gOm17jCoZ9jMxvL haylin@haytop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKsUHsNfWi9qEivDXP146uGBnW2H1m4tOW+An0b3MkZ infra@hayl.in"
       ];
     };
     haylin = {
