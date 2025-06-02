380f479c4176bef845c68ab02f3909639534fc83
Haylin Moore <me@haylinmoore.com>
2025-06-02 08:45:23 -0700

    modules(proxy-sites): init

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./dollpublish.nix
     ./static-sites.nix
+    ./proxy-sites.nix
     ./www.nix
   ];
 }
---
+++ modules/nixos/proxy-sites.nix
@@ -0,0 +1,55 @@
+{ config, lib, pkgs, ... }:
+
+let
+  mkProxySite = name: cfg: {
+    inherit name;
+    value = {
+      forceSSL = cfg.ssl;
+      enableACME = cfg.ssl;
+      locations = {
+        "/" = {
+          proxyPass = "${cfg.proxyUri}";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto https;
+          '';
+        };
+      };
+    };
+  };
+in
+
+{
+  options.proxySites = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule {
+      options = {
+        domain = lib.mkOption {
+          type = lib.types.str;
+          description = "The domain for this proxy site.";
+        };
+
+        proxyUri = lib.mkOption {
+          type = lib.types.str;
+          description = "The connection Uri for this proxy site.";
+        };
+
+        ssl = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Whether to enable SSL for this proxy site.";
+        };
+      };
+    });
+    default = {};
+    description = "Proxy sites to serve with nginx.";
+  };
+
+  config = lib.mkIf (config.proxySites != {}) {
+    services.nginx.enable = true;
+    services.nginx.virtualHosts = lib.listToAttrs (
+      lib.mapAttrsToList (name: cfg: mkProxySite cfg.domain cfg) config.proxySites
+    );
+  };
+}
