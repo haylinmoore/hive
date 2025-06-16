eee0a084d47fcd5b63225a1d5110a5d04661662c
Haylin Moore <me@haylinmoore.com>
2025-06-16 15:27:33 -0700

    yggdrasil(bella): init as 204:710b:f568:ec5e:1769:f461:9498:cb6d

--- modules/hosts/bella/services.nix
+++ modules/hosts/bella/services.nix
@@ -69,4 +69,16 @@ in
     port = 15641;
   };
 
+  services.yggdrasil = {
+    enable = true;
+    persistentKeys = true;
+    settings = {
+      Peers = [
+        "tcp://longseason.1200bps.xyz:13121"
+        "tcp://ygg-pa.incognet.io:8883"
+        "tcp://ygg-kcmo.incognet.io:8883"
+      ];
+    };
+  };
+
 }
