e0c1aaa62d0fc50e23c8af7340c35d31ab1ff0ca
Haylin Moore <me@haylinmoore.com>
2025-06-22 12:00:50 -0700

    zoe(ha): pull upstairs lights into nix model

--- modules/hosts/zoe/services.nix
+++ modules/hosts/zoe/services.nix
@@ -27,7 +27,12 @@
       "zha"
       "homekit"
     ];
-    extraPackages = python3Packages: with python3Packages; [ ];
+    extraPackages =
+      python3Packages: with python3Packages; [
+        # Mentioned in crashes
+        aiohomekit
+        python-otbr-api
+      ];
     config = {
       default_config = { };
       http = {
@@ -39,6 +44,81 @@
         use_x_forwarded_for = true;
       };
       "automation ui" = "!include automations.yaml";
+      "automation manual" =
+        let
+          upstairsButtons = [
+            "c1fb487579953c8e36567d604e906381"
+            "8f9f8c0d2ecabaf1a59632e6f38970dc"
+          ];
+
+          makeButtonTriggers =
+            triggerType:
+            map (device_id: {
+              inherit device_id;
+              domain = "zha";
+              type = triggerType;
+              subtype = "button";
+              trigger = "device";
+            }) upstairsButtons;
+        in
+        [
+          {
+            alias = "Upstairs Toggle Lights";
+            triggers = makeButtonTriggers "remote_button_short_press";
+            actions = [
+              {
+                action = "light.toggle";
+                target = {
+                  entity_id = "light.upstairs_lights";
+                };
+              }
+            ];
+            mode = "single";
+          }
+          {
+            alias = "Upstairs Dim Lights";
+            triggers = makeButtonTriggers "remote_button_long_press";
+            actions = [
+              {
+                action = "light.turn_off";
+                data = {
+                  transition = 1;
+                };
+                target = {
+                  entity_id = "light.room_lamp";
+                };
+              }
+              {
+                action = "light.turn_on";
+                data = {
+                  brightness_pct = 25;
+                  transition = 1;
+                };
+                target = {
+                  entity_id = "light.stair_lights";
+                };
+              }
+            ];
+            mode = "single";
+          }
+          {
+            alias = "Upstairs Normalize Lights";
+            description = "";
+            triggers = makeButtonTriggers "remote_button_double_press";
+            actions = [
+              {
+                action = "light.turn_on";
+                data = {
+                  brightness_pct = 100;
+                };
+                target = {
+                  area_id = "bedroom";
+                };
+              }
+            ];
+            mode = "single";
+          }
+        ];
     };
   };
   systemd.tmpfiles.rules = [
