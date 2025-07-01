7a5f8647b8008ff1e1f4e511e0faf10c45891e6a
Haylin Moore <me@haylinmoore.com>
2025-07-01 11:03:59 -0700

    bella(www): add ygg.hayl.in domain

--- modules/hosts/bella/services.nix
+++ modules/hosts/bella/services.nix
@@ -9,7 +9,7 @@ let
   sources = import ../../../npins;
 in
 
-{
+rec {
   imports = [
     ./music
   ];
@@ -77,6 +77,12 @@ in
     enable = true;
     domain = "hayl.in";
     port = 15641;
+  };
+
+  proxySites.ygg-haylin = {
+    domain = "ygg.hayl.in";
+    proxyUri = "http://localhost:${toString www.port}/";
+    ssl = false;
   };
 
   services.yggdrasil = {
