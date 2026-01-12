de4a92ce31669536f73f264043d49883e3136d91
Haylin Moore <hmoore@qumulo.com>
2026-01-12 09:00:43 -0800

    nixos(shared): add hmoore ssh key

--- nixos/shared/users.nix
+++ nixos/shared/users.nix
@@ -11,6 +11,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHavg+rhFmR2p9wuWiO4VxKaIXpq1gOm17jCoZ9jMxvL haylin@haytop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKsUHsNfWi9qEivDXP146uGBnW2H1m4tOW+An0b3MkZ infra@hayl.in"
+	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILXL2etr9int91HoWiyh8P4+UJH/nb2i5KSJ+tBb+BDi hmoore@qumulo.com"
       ];
     };
     haylin = {
