2b95b725e4b16ee61a964a228331059398d0dd09
Haylin Moore <me@haylinmoore.com>
2026-02-28 20:06:43 -0800

    umaring: proxy umaring.github.io

--- hosts/athena/umaring.nix
+++ hosts/athena/umaring.nix
@@ -5,62 +5,17 @@
   hive,
   ...
 }:
-let
-  port = "3432";
-in
 {
-  systemd.services.umaring = {
-    description = "Umaring service";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-
-    environment = {
-      BIND = "127.0.0.1:${port}";
-    };
-
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${hive.web.umaring}/bin/umaring";
-      Restart = "always";
-      RestartSec = 5;
-
-      # Security hardening
-      DynamicUser = true;
-      NoNewPrivileges = true;
-      PrivateTmp = true;
-      ProtectSystem = "strict";
-      ProtectHome = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectControlGroups = true;
-      RestrictAddressFamilies = [
-        "AF_INET"
-        "AF_INET6"
-      ];
-      RestrictNamespaces = true;
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
-      SystemCallFilter = "@system-service";
-      SystemCallErrorNumber = "EPERM";
-    };
-  };
-
-  # Nginx configuration for umaring.mkr.cx
   services.nginx = {
     enable = true;
     virtualHosts."umaring.mkr.cx" = {
       forceSSL = true;
       enableACME = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${port}";
-        proxyWebsockets = true;
+        proxyPass = "https://umaring.github.io";
         extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_ssl_server_name on;
+          proxy_set_header Host umaring.github.io;
         '';
       };
     };
--- web/umaring.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, hive, ... }:
-
-let
-  naersk = pkgs.callPackage hive.sources.naersk.outPath { };
-in
-naersk.buildPackage {
-  src = hive.sources.umaring.outPath;
-}
