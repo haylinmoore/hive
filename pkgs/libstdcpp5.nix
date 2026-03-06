623a0e135a721d176c5fb155702e3aa5d356b220
Haylin Moore <me@haylinmoore.com>
2026-03-06 10:55:49 -0800

    pkgs/libstdcpp5: pull in derivation from bunbun

--- pkgs/libstdcpp5.nix
+++ pkgs/libstdcpp5.nix
@@ -9,14 +9,16 @@ stdenv.mkDerivation {
   pname = "libstdc++5";
   version = "3.3.6";
   src = fetchurl {
-    url = "https://archive.debian.org/debian/pool/main/g/gcc-3.3/libstdc++5_3.3.6-15_i386.deb";
-    sha256 = "sha256-NCQwRGYjK7XHufw10t6Pu7go2LPv4Y1RIVjpAuIYhDI=";
+    url = "https://archive.debian.org/debian/pool/main/g/gcc-3.3/libstdc++5_3.3.6-32_i386.deb";
+    sha256 = "sha256-6lIy8C5wjsGHbVU5rJW2QHrqCJVPWRSqVI31xm+mIpQ=";
   };
-  nativeBuildInputs = [ dpkg ];
-  dontUnpack = true;
+
+  unpackPhase = ''
+    ${dpkg}/bin/dpkg -x $src .
+  '';
+
   installPhase = ''
     mkdir -p $out/lib
-    dpkg-deb -x $src unpacked
-    find unpacked -name '*.so*' -exec cp -a {} $out/lib/ \;
+    cp ./usr/lib/*/*.so.5 $out/lib
   '';
 }
